48536fe6cfbb829ad09f0f8506866b93a0697e3c
Katharine <katharine.risingphoenix@gmail.com>
2025-04-26 13:56:37 -0600

    Name prompt and name display written

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //name prompt//
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        //Display name//
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
