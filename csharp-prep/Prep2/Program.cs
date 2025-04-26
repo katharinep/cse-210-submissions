8e8a0ff5381117599f9c5ceb2e731e0d502b7f84
Katharine <katharine.risingphoenix@gmail.com>
2025-04-26 14:45:01 -0600

    Added grade conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,5 +11,30 @@ class Program
         string userInput = Console.ReadLine();
         int grade = int.Parse(userInput);
         //Console.WriteLine(grade);//
+
+        //grade conditionals//
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. :(");
+        }
     }
 }
