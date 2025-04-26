909cc5e559a52c800bc0a8709dbf33fcab606c66
Katharine <katharine.risingphoenix@gmail.com>
2025-04-26 14:37:52 -0600

    Grade prompt added and debugged

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        //grade prompt//
+        Console.WriteLine("What is the grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        //Console.WriteLine(grade);//
     }
 }
