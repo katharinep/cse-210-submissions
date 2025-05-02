f41e3fa7fcc304487befb295122e3548bc1b05b3
Katharine <katharine.risingphoenix@gmail.com>
2025-05-01 19:38:46 -0600

    Added if statements comparing numbers

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,15 +7,33 @@ class Program
         //Console.WriteLine("Hello Prep3 World!");//
 
         //ask for magic number//
-        Console.WriteLine("What is the magic number? ");
+        Console.WriteLine("What is the magic number?");
         string userInput = Console.ReadLine();
         int magicNumber = int.Parse(userInput);
         //Console.WriteLine(magicNumber);//
 
         //Guess the number//
-        Console.WriteLine("What is your guess? ");
+        Console.WriteLine("What is your guess?");
         string userGuess = Console.ReadLine();
         int guess = int.Parse(userGuess);
         Console.WriteLine(guess);
+
+        //if statement comparing numbers//
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
     }
 }
