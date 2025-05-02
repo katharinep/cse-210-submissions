b24c743cfb1029a77217a376f674d1d877db0ec8
Katharine <katharine.risingphoenix@gmail.com>
2025-05-01 19:43:32 -0600

    added while loop to keep guesses going until the number is guessed (debugged)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,34 +6,40 @@ class Program
     {
         //Console.WriteLine("Hello Prep3 World!");//
 
+        bool game = true;
         //ask for magic number//
         Console.WriteLine("What is the magic number?");
         string userInput = Console.ReadLine();
         int magicNumber = int.Parse(userInput);
         //Console.WriteLine(magicNumber);//
 
-        //Guess the number//
-        Console.WriteLine("What is your guess?");
-        string userGuess = Console.ReadLine();
-        int guess = int.Parse(userGuess);
-        Console.WriteLine(guess);
+        //while loop for guesses//
+        while (game == true)
+        {
+            //Guess the number//
+            Console.WriteLine("What is your guess?");
+            string userGuess = Console.ReadLine();
+            int guess = int.Parse(userGuess);
+            //Console.WriteLine(guess);//
 
-        //if statement comparing numbers//
-        if (guess == magicNumber)
-        {
-            Console.WriteLine("You guessed it!");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (guess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("Please enter a valid number.");
+            //if statement comparing numbers//
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                game = false;
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
         }
     }
 }
