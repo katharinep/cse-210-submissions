cdecdae38a4a5a51daf9cd37bf575d2276e11698
Katharine <katharine.risingphoenix@gmail.com>
2025-05-01 20:11:08 -0600

    Attempt to add play again while loop (it's borked)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Prep3 World!");//
-
+        bool playAgain = true;
         bool game = true;
         int count = 0;
         //ask for magic number//
@@ -14,42 +14,60 @@ class Program
         //int magicNumber = int.Parse(userInput);
         //Console.WriteLine(magicNumber);//
 
-        //random number//
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        //while loop for guesses//
-        while (game == true)
+        while (playAgain == true)
         {
-            //Guess the number//
-            Console.WriteLine("What is your guess? (1-100)");
-            string userGuess = Console.ReadLine();
-            int guess = int.Parse(userGuess);
-            //Console.WriteLine(guess);//
-            count = count + 1;
+            //random number//
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            //if statement comparing numbers//
-            if (guess > magicNumber)
+            //while loop for guesses//
+            while (game == true)
             {
-                Console.WriteLine("Lower");
-            }
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-                game = false;
-                if (count == 1)
+                //Guess the number//
+                Console.WriteLine("What is your guess? (1-100)");
+                string userGuess = Console.ReadLine();
+                int guess = int.Parse(userGuess);
+                //Console.WriteLine(guess);//
+                count = count + 1;
+
+                //if statement comparing numbers//
+                if (guess > magicNumber)
                 {
-                    Console.WriteLine("You guessed in one try!");
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
                 }
                 else
                 {
-                    Console.WriteLine($"You guessed in {count} tries!");
+                    Console.WriteLine("You guessed it!");
+                    game = false;
+                    if (count == 1)
+                    {
+                        Console.WriteLine("You guessed in one try!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed in {count} tries!");
+                    }
                 }
             }
+        }     
+        //Ask to play again//
+        Console.WriteLine("Would you like to play again? (yes/no)");
+        string playResponse = Console.ReadLine();
+        if (playResponse == "yes")
+        {
+            playAgain = true;
+        }
+        else if (playResponse == "no")
+        {
+            playAgain = false;
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid response.");
         }
     }
 }
