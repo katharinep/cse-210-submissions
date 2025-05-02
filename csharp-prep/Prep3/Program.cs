913a426178a9e6db05a703d841211134d821d09a
Katharine <katharine.risingphoenix@gmail.com>
2025-05-01 19:32:47 -0600

    created user input for magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");//
+
+        //ask for magic number//
+        Console.WriteLine("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        Console.WriteLine(magicNumber);
     }
 }
