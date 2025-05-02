e0788c833273b833adaf57b677d2406fb41727e2
Katharine <katharine.risingphoenix@gmail.com>
2025-05-01 19:35:03 -0600

    Added user guess (not looped)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,6 +10,12 @@ class Program
         Console.WriteLine("What is the magic number? ");
         string userInput = Console.ReadLine();
         int magicNumber = int.Parse(userInput);
-        Console.WriteLine(magicNumber);
+        //Console.WriteLine(magicNumber);//
+
+        //Guess the number//
+        Console.WriteLine("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+        Console.WriteLine(guess);
     }
 }
