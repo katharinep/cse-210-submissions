b78d0abadecae91384d6f061f4c63e3c37edf2b9
Katharine <katharine.risingphoenix@gmail.com>
2025-05-06 02:43:11 -0600

    Started while loop for collecting numbers.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");//
+
+        bool notZero = true;
+
+        while (notZero == true)
+        {
+            //Ask for list of numbers.
+            Console.WriteLine("Enter number for the list. Enter 0 when done.");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            Console.WriteLine("You entered" + number);
+
+        }
     }
 }
