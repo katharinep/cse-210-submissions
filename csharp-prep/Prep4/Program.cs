a8fd5c21052e0ad484482cc54c87d491b2b0b3d5
Katharine <katharine.risingphoenix@gmail.com>
2025-05-06 02:45:39 -0600

    Finished while loop for entering 0

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -14,8 +14,13 @@ class Program
             Console.WriteLine("Enter number for the list. Enter 0 when done.");
             string userInput = Console.ReadLine();
             int number = int.Parse(userInput);
-            Console.WriteLine("You entered" + number);
+            Console.WriteLine("You entered " + number);
 
+            if (number == 0)
+            {
+                notZero = false;
+            }
         }
+        Console.WriteLine("Loop ended.");
     }
 }
