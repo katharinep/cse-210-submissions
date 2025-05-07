0dbb03b9e4e2b56141e3395ebbca5e8a8f566797
Katharine <katharine.risingphoenix@gmail.com>
2025-05-07 17:48:42 -0600

    First two functions written

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");//
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welocome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            return userName;
+        }
     }
 }
