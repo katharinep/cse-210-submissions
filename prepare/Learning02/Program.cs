cea9c814f9ebdad8f5e83b2bdaa9971fd4eddfd3
Katharine <katharine.risingphoenix@gmail.com>
2025-05-07 19:25:30 -0600

    Created first job. no bugs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,6 +7,12 @@ class Program
         //Console.WriteLine("Hello Learning02 World!");//
 
         Job job1 = new Job();
-        
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2001;
+        job1._endYear = 2008;
+
+        job1.Display();
     }
 }
