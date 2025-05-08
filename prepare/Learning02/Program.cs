a2be84822013ee81065659ae7ae4f0fb09415df8
Katharine <katharine.risingphoenix@gmail.com>
2025-05-07 19:23:14 -0600

    Started Job class. Wrote dispaly method. Started constructor

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");//
+
+        Job job1 = new Job();
+        
     }
 }
