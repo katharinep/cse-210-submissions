baca8f2df0e9c3f3d453890da2340965bab3e64e
Katharine <katharine.risingphoenix@gmail.com>
2025-05-13 16:57:47 -0600

    Created class variables and constructor for Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,4 +1,16 @@
 public class Entry
 {
-    
+    public string _entry;
+    public string _prompt;
+    public string _date;
+
+    //constructor//
+    public Entry(string entry, string prompt, string date)
+    {
+        _entry = entry;
+        _prompt = prompt;
+        _date = date;
+    }
+
+    //Methods//
 }
