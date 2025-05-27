dbff0fd5e102b579517730eda730ab42e73595c7
Katharine <katharine.risingphoenix@gmail.com>
2025-05-26 20:04:52 -0600

    Finished coding the getters and setters for Fraction class

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -11,14 +11,34 @@ public class Fraction
 
     public Fraction(int wholeNumber)
     {
-
+        _top = wholeNumber;
+        _bottom = 1;
     }
 
     public Fraction(int top, int bottom)
     {
-
+        _top = top;
+        _bottom = bottom;
     }
 
     //getters and setters
-    
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
 }
