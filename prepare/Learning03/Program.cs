eece2dcc802ad93692cc528a1564ad6c6713d3a8
Katharine <katharine.risingphoenix@gmail.com>
2025-05-26 20:16:43 -0600

    Created user input to set top and bottom in main

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Fraction Calculator");
+        Console.Write("Enter the top number: ");
+        string userTop = Console.ReadLine();
+        int top = int.Parse(userTop);
+
+        Console.Write("Enter the bottom number: ");
+        string userBottom = Console.ReadLine();
+        int bottom = int.Parse(userBottom);
+        
     }
 }
