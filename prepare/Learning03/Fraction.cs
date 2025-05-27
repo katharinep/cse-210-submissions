e738a818436a47f2833ac41522d6090ad49e639d
Katharine <katharine.risingphoenix@gmail.com>
2025-05-26 20:00:20 -0600

    Initial set up of Fraction class. variables and basic constructors

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,24 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    //constructors
+    public Fraction()
+    {
+
+    }
+
+    public Fraction(int wholeNumber)
+    {
+
+    }
+
+    public Fraction(int top, int bottom)
+    {
+
+    }
+
+    //getters and setters
+    
+}
