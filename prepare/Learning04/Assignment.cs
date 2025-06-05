35b7cb15a2c296c888c8cda0b9ddd8e591b67888
Katharine <katharine.risingphoenix@gmail.com>
2025-06-04 20:08:09 -0600

    Created Assignment.cs and tested it.

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,37 @@
+using System;
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment()
+    {
+        string studentname = _studentName;
+        string topic = _topic;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public void SetStudentName(string name)
+    {
+        _studentName = name;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");//
+
+        Assignment assignment1 = new Assignment();
+        assignment1.SetStudentName("Bob");
+        assignment1.SetTopic("Fractions");
+
+        Console.WriteLine(assignment1.GetSummary());
     }
 }
