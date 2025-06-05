7446b01f9cbac81a5ccb2c6de213cf6bc9e1fe2c
Katharine <katharine.risingphoenix@gmail.com>
2025-06-05 16:56:06 -0600

    Tested math class.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -9,5 +9,9 @@ class Program
         Assignment assignment1 = new Assignment("Bob", "Fractions");
 
         Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("7.3", "8-19", "Roberto Rodriquez", "Fractions");
+        string mathsummary = math1.GetHomeworkList();
+        Console.WriteLine(mathsummary);
     }
 }
