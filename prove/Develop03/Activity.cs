52617ecf97e9dc8f19ab1c8ac29ac20fdaceff8e
Katharine <katharine.risingphoenix@gmail.com>
2025-06-09 22:32:43 -0600

    Added getters, setters, and basic syntax for methods.

--- prove/Develop03/Activity.cs
+++ prove/Develop03/Activity.cs
@@ -5,10 +5,56 @@ public class Activity
 
     public Activity()
     {
-
+        duration = _duration;
+        activitychoice = _activityChoice;
     }
 
     //Getters and Setters
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public int GetActivityChoice()
+    {
+        return _activityChoice;
+    }
+
+    public void SetDuration()
+    {
+        duration = _duration;
+    }
+
+    public void SetActivityChoice()
+    {
+        activitychoice = _activityChoice;
+    }
 
     //Methods
+
+    public void ActivityDescription(int _activityChoice)
+    {
+        //display activity instructions with conditional reasoning
+    }
+
+    public void ActivityTimer(int _duration)
+    {
+        //activity timer
+    }
+
+    public void EndingMessage()
+    {
+        //ending message
+    }
+
+    public void Spinner()
+    {
+        //display spinner animation
+    }
+
+    public void ClearScreen()
+    {
+        Console.Clear();
+    }
+
 }
