393b796b6d9b309934ccfc0b3c44f3f56552d94e
Katharine <katharine.risingphoenix@gmail.com>
2025-06-11 17:23:49 -0600

    wrote menu display method

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -11,5 +11,13 @@ class Program
     public void DisplayMenu()
     {
         //menu//
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflection activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        Console.Write("Select a choice from the menu: ");
+
+
     }
 }
