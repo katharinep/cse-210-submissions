12aa8016b72dcf6655b92dd2927e10c63c00dec2
Katharine <katharine.risingphoenix@gmail.com>
2025-06-12 15:44:45 -0600

    The Mindfullness Program was created in Dev03 instead of Dev04. This commit is intended to move the files to the correct folder.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        //Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to my Mindfullness Program");
+
+        
+    }
+
+    public void DisplayMenu()
+    {
+        //menu//
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflection activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        Console.Write("Select a choice from the menu: ");
     }
 }
