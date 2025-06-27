8f95ec3ed7a5b7ca945a3974409574fa52783497
Katharine <katharine.risingphoenix@gmail.com>
2025-06-27 15:30:09 -0600

    Added menu methods to Menu class and added conditional logic and basic function of program in Main.

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -2,11 +2,25 @@ public class Menu
 {
     public void ShowMenu()
     {
-
+        Console.WriteLine("Menu Options: ");
+        Console.WriteLine("1. Create New Goal");
+        Console.WriteLine("2. List Goals");
+        Console.WriteLine("3. Save Goals");
+        Console.WriteLine("4. Load Goals");
+        Console.WriteLine("5. Record Event");
+        Console.WriteLine("6. Quit");
+        Console.WriteLine("");
+        Console.Write("Select a choice from the menu: ");
     }
 
     public void ShowGoals()
     {
-        
+        Console.WriteLine("The type of goals are:");
+        Console.WriteLine("");
+        Console.WriteLine("1. Simple Goal");
+        Console.WriteLine("2. Eternal Goal");
+        Console.WriteLine("3. Checklist Goal");
+        Console.WriteLine("");
+        Console.WriteLine("Which type would you like to create?");
     }
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Goal Tracker");
+        int userPoints = 0;
+        bool quit = false;
+
+        while (quit == false)
+        {
+            Console.WriteLine($"You have {userPoints} points");
+            Menu menu = new Menu();
+            menu.ShowMenu();
+            string input = Console.ReadLine();
+            int menuChoice = int.Parse(input);
+
+            if (menuChoice >= 1 && menuChoice <= 6)
+            {
+                if (menuChoice == 1)
+                {
+                    //New Goal
+                }
+                else if (menuChoice == 2)
+                {
+                    //List Goal
+                }
+                else if (menuChoice == 3)
+                {
+                    //Save Goal
+                }
+                else if (menuChoice == 4)
+                {
+                    //Load Goal
+                }
+                else if (menuChoice == 5)
+                {
+                    //Record Event
+                }
+                else
+                {
+                    Console.WriteLine("See you next time!");
+                    quit = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please choose an option between 1 and 6");
+                quit = false;
+            }
+        }
     }
 }
