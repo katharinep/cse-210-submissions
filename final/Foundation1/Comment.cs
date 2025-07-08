a848458b57bec0944ec0e1672504ae49ab8f27c6
Katharine <katharine.risingphoenix@gmail.com>
2025-07-08 13:35:00 -0600

    Initial commit. Set up basic attributes and behaviors (empty) according to class diagram.

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,16 @@
+public class Comment
+{
+    private string _comment;
+    private string _userName;
+    private int _commentCount;
+
+    public Comment()
+    {
+
+    }
+
+    public int CommentCount()
+    {
+        return 0;
+    }
+}
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,17 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<string> _comment;
+
+    public Video()
+    {
+
+    }
+
+    public void DisplayVideo()
+    {
+        
+    }
+}
