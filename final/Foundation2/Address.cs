1fcd26b19d19ec961f041a327f394f126c279203
Katharine <katharine.risingphoenix@gmail.com>
2025-07-10 22:10:59 -0600

    Moved InUsa method to the Customer class. Created address instance in class to access the country variable.

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -5,12 +5,21 @@ public class Address
     private string _state;
     private string _country;
 
+    public Address()
+    {
+        
+    }
     public Address(string street, string city, string state, string country)
     {
         _street = street;
         _city = city;
         _state = state;
         _country = country;
+    }
+
+    public string GetCountry()
+    {
+        return _country;
     }
 
     public string AddressString()
--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,16 +1,29 @@
 public class Customer
 {
     private string _customerName;
-    private Address _customerAddress;
+    private Address _customerAddress = new Address();
 
     public Customer(string customername, Address customeraddress)
     {
         _customerName = customername;
-        _customerAddress = customeraddress;
+        _customerAddress = new Address();
     }
 
     public string CustomerString()
     {
         return $"{_customerName}\n{_customerAddress}";
+    }
+
+    public bool InUsa()
+    {
+        string country = _customerAddress.GetCountry();
+        if (country == "USA")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
