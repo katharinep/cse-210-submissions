71a0ae1b02418ca8e7e166c0c42e85acb6e77cd0
Katharine <katharine.risingphoenix@gmail.com>
2025-07-10 20:56:26 -0600

    Coded Address class. Just debugging

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -5,18 +5,28 @@ public class Address
     private string _state;
     private string _country;
 
-    public Address()
+    public Address(string street, string city, string state, string country)
     {
-
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
     public string AddressString()
     {
-        return "";
+        return $"{_street} {_city}, {_state} {_country}";
     }
 
     public bool IsUsa()
     {
-        return false;
+        if (_country == "USA")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
