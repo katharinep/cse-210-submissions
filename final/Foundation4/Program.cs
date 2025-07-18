68a80e0a78b86a0208ddcbb098305201622f1a89
Katharine <katharine.risingphoenix@gmail.com>
2025-07-18 17:53:59 -0600

    Added required instances. Final commit.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,19 +7,32 @@ class Program
         //Running Activity 1
         Running running1 = new Running(new DateTime(2025, 7, 1), 24, 5);
         Console.WriteLine(running1.DisplaySummary());
+        Console.WriteLine("");
 
         //Running Activity 2
         Running running2 = new Running(new DateTime(2025, 7, 2), 22, 4);
         Console.WriteLine(running2.DisplaySummary());
-        
+        Console.WriteLine("");
+
         //Swimming Activity 1
+        Swimming swimming1 = new Swimming(new DateTime(2025,7,3),21,22);
+        Console.WriteLine(swimming1.DisplaySummary());
+        Console.WriteLine("");
 
         //Swimming Activity 2
+        Swimming swimming2 = new Swimming(new DateTime(2025,7,4),20,20);
+        Console.WriteLine(swimming2.DisplaySummary());
+        Console.WriteLine("");
 
         //Biking Activity 1
+        Biking biking1 = new Biking(new DateTime(2025,7,5),65,21);
+        Console.WriteLine(biking1.DisplaySummary());
+        Console.WriteLine("");
 
         //Biking Activity 2
-
+        Biking biking2 = new Biking(new DateTime(2025,7,6),72,20);
+        Console.WriteLine(biking2.DisplaySummary());
+        Console.WriteLine("");
     }
 }
 //Math Hints:
