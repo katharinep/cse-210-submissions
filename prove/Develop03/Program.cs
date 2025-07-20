daf9f3c2d1f7490d87be3e39d4ca66c8e518eacc
Katharine <katharine.risingphoenix@gmail.com>
2025-07-20 14:17:28 -0600

    Main written

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,13 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Scripture Memory!\n\n");
-        
-        bool memory = false;
-        while (memory == false)
+
+        bool memory = true;
+        while (memory == true)
         {
             Reference reference = Reference.RandomScripture();
             Scripture scripture = new Scripture(reference);
 
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetRenderedText());
+
+                if (scripture.AllHidden())
+                {
+                    break;
+                }
+
+                Console.WriteLine("\nPress enter to continue or press 'q' to quit.\n");
+                string userinput = Console.ReadLine();
+
+                if (userinput.Trim().ToLower() == "q")
+                {
+                    break;
+                }
+
+                scripture.HideWords();
+            }
+
+            Console.WriteLine("\nWould you like to try another scripture? (y/n)\n");
+
+            string input = Console.ReadLine();
+            if (input.Trim().ToLower() != "y")
+            {
+                break;
+            }
         }
     }
 
